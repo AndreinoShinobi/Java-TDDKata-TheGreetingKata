f4f5013683da662d2a609dd1b3f91a70658a7364
Team Ninja <ninja@trovaprezzi.it>
2017-11-17 18:00:52 +0100

    6 scenari

--- Greeting/src/greeting/Greeting.java
+++ Greeting/src/greeting/Greeting.java
@@ -1,25 +1,109 @@
 package greeting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Greeting {
 
-    public static final String defaultGreet = "Hello, my friend.";
+    public static final String defaultGreet = "Hello my friend.";
 
-    public static String greet(String name){
-        if(name==null)
-            return defaultGreet;
 
-        if(name.toUpperCase().equals(name))
-            return "HELLO, " + name + "!";
+    public static String greet(Object text){
+
+        if(text == null) return defaultGreet;
+
+        if(text.getClass() == String.class)
+            return singleGreeting((String)text);
+
+        if(text.getClass().isArray())
+            return multipleGreetings((String[])text);
+
+        return "Wrong input. Cannot Greet :(";
+    }
+
+    private static String singleGreeting(String name) {
+
+        if (isAllUpperCase(name))
+            return buildBaseUpperCaseGreet(name);
 
         return buildBaseGreet(name);
     }
 
-    public static String greet(String[] names){
-        String namesList = String.join(" and ",names);
-        return buildBaseGreet(namesList);
+    private static String multipleGreetings(String[] names) {
+
+        if (arrayHasDownCaseNamesOnly(names)) {
+            return buildDownCaseNamesGreetings(names);
+        }
+
+        return buildUpperAndDownCaseNamesGreetings(names);
     }
 
-    private static String buildBaseGreet(String nameString){
-        return "Hello, " + nameString + ".";
+    private static String buildBaseGreet(String nameString) {
+        return "Hello " + nameString + ".";
+    }
+
+    private static String buildBaseUpperCaseGreet(String nameString) {
+        return "HELLO " + nameString + "!";
+    }
+
+    private static String buildDownCaseNamesGreetings(String[] names){
+        if (names.length == 2) {
+            String namesList = String.join(" and ", names);
+            return buildBaseGreet(namesList);
+        }
+
+        return buildMoreOfTwoGreet(names);
+    }
+
+    private static String buildUpperAndDownCaseNamesGreetings(String[] names) {
+        ArrayList<String> downCaseNames = new ArrayList<>();
+        ArrayList<String> upperCaseNames = new ArrayList<>();
+
+        for (String name : names) {
+            if (!isAllUpperCase(name)) {
+                downCaseNames.add(name);
+            } else {
+                upperCaseNames.add(name);
+            }
+        }
+
+        String[] downCaseNamesArray = new String[downCaseNames.size()];
+        downCaseNames.toArray(downCaseNamesArray);
+        String[] upperCaseNamesArray = new String[upperCaseNames.size()];
+        upperCaseNames.toArray(upperCaseNamesArray);
+
+
+        String downCaseGreetings = (downCaseNames.size()>1) ? buildMoreOfTwoGreet(downCaseNamesArray) : buildBaseGreet(downCaseNamesArray[0]);
+        String upperCaseGreetings = (upperCaseNames.size()>1) ? buildMoreOfTwoGreetUpperCase(upperCaseNamesArray) : buildBaseUpperCaseGreet(upperCaseNamesArray[0]);
+
+        return downCaseGreetings + " AND " + upperCaseGreetings;
+    }
+
+    private static String buildMoreOfTwoGreet(String[] names) {
+
+        int lastElementIndex = names.length - 1;
+        String commonSeparatedNames = String.join(", ", Arrays.copyOf(names, lastElementIndex));
+        String lastName = names[lastElementIndex];
+        return buildBaseGreet(commonSeparatedNames + " and " + lastName);
+    }
+
+    private static String buildMoreOfTwoGreetUpperCase(String[] names) {
+
+        int lastElementIndex = names.length - 1;
+        String commonSeparatedNames = String.join(", ", Arrays.copyOf(names, lastElementIndex));
+        String lastName = names[lastElementIndex];
+        return buildBaseUpperCaseGreet(commonSeparatedNames + " AND " + lastName);
+    }
+
+    private static boolean arrayHasDownCaseNamesOnly(String[] names) {
+        for (String name : names) {
+            if (isAllUpperCase(name))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isAllUpperCase(String text) {
+        return (text.toUpperCase().equals(text));
     }
 }
--- Greeting/src/greeting/GreetingTest.java
+++ Greeting/src/greeting/GreetingTest.java
@@ -2,28 +2,40 @@ package greeting;
 
 
 import org.testng.annotations.Test;
+
 import static org.testng.AssertJUnit.assertEquals;
 
-public class GreetingTest{
+public class GreetingTest {
 
     @Test
     public void baseGreet() {
-        assertEquals("Hello, Bob.",Greeting.greet("Bob"));
+        assertEquals("Hello Bob.", Greeting.greet("Bob"));
     }
 
     @Test
     public void shouldReturnDefaultGreetWhenNameIsNull() {
-        assertEquals("Hello, my friend.",Greeting.greet((String)null));
+        assertEquals("Hello my friend.", Greeting.greet( null));
     }
 
     @Test
     public void shouldShoutWOnAllUpperCaseName() {
-        assertEquals("HELLO, BOB!", Greeting.greet("BOB"));
+        assertEquals("HELLO BOB!", Greeting.greet("BOB"));
+    }
+
+    @Test
+    public void shouldGreetTwoFriends() {
+        assertEquals("Hello Jane and Mary.", Greeting.greet(new String[]{"Jane", "Mary"}));
     }
 
     @Test
     public void shouldGreetMultipleFriends() {
-        assertEquals("Hello, Jane and Mary.", Greeting.greet(new String[]{"Jane", "Mary"}));
+        assertEquals("Hello Dave, Jane, Max and Mary.", Greeting.greet(new String[]{"Dave", "Jane", "Max", "Mary"}));
     }
 
+    @Test
+    public void shouldGreetBothShoutAndQuiet() {
+        assertEquals("Hello Dave, Max and Mary. AND HELLO JANE!", Greeting.greet(new String[]{"Dave", "JANE", "Max", "Mary"}));
+        assertEquals("Hello Dave and Mary. AND HELLO JANE AND MAX!", Greeting.greet(new String[]{"Dave", "JANE", "MAX", "Mary"}));
+        assertEquals("Hello Dave. AND HELLO JANE, MAX AND MARY!", Greeting.greet(new String[]{"Dave", "JANE", "MAX", "MARY"}));
+    }
 }
